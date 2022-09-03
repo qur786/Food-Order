de31b5e89cce4bc50b710b04906213c9c3bf08fe
Qurban Ahmad <ahmad76qurban@gmail.com>
2022-09-03 17:46:45 +0530

    feat: add AvailableMeals component

---
+++ src/components/AvailableMeals/index.tsx
@@ -0,0 +1,51 @@
+import classes from "./index.module.css";
+
+const DUMMY_MEALS = [
+  {
+    id: "m1",
+    name: "Sushi",
+    description: "Finest fish and veggies",
+    price: 22.99,
+  },
+  {
+    id: "m2",
+    name: "Schnitzel",
+    description: "A german specialty!",
+    price: 16.5,
+  },
+  {
+    id: "m3",
+    name: "Barbecue Burger",
+    description: "American, raw, meaty",
+    price: 12.99,
+  },
+  {
+    id: "m4",
+    name: "Green Bowl",
+    description: "Healthy...and green...",
+    price: 18.99,
+  },
+];
+
+interface Meal {
+  id: string;
+  name: string;
+  description: string;
+  price: number;
+}
+
+interface AvailableMealsProps {
+  meals: Meal[];
+}
+
+export function AvailableMeals({ meals }: AvailableMealsProps) {
+  return (
+    <section className={classes.meals}>
+      <ul>
+        {meals.map((meal) => (
+          <li>{meal.name}</li>
+        ))}
+      </ul>
+    </section>
+  );
+}
