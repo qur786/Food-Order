f657b73bf29f0a295e49d00b2a4183e64d651cd9
Qurban Ahmad <ahmad76qurban@gmail.com>
2022-09-03 22:45:18 +0530

    feat: add Cart to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Fragment } from "react";
 import { Header } from "./components/Header";
 import { Meals } from "./components/Meals";
+import { Cart } from "./components/Cart";
 
 function App() {
   return (
     <Fragment>
+      <Cart />
       <Header />
       <main>
         <Meals />
