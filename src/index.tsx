c50d93ae469ddc17bdc82f60ae948f4cb9ab04c9
Qurban Ahmad <ahmad76qurban@gmail.com>
2022-09-04 17:11:46 +0530

    refactor: fix multiple dispatch call issue

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,11 @@
-import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 const root = createRoot(document.getElementById("root")!);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Ref: https://stackoverflow.com/questions/54892403/usereducer-action-dispatched-twice
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
