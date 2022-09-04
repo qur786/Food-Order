db31aea4366f5f6cac79c258bda2f5241ad69ec3
Qurban Ahmad <ahmad76qurban@gmail.com>
2022-09-04 12:10:59 +0530

    feat: add validation to MenuItemForm

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,15 +1,18 @@
-import type { InputHTMLAttributes } from "react";
+import { forwardRef } from "react";
+import type { InputHTMLAttributes, Ref } from "react";
 import classes from "./index.module.css";
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   label: string;
 }
 
-export function Input(props: InputProps) {
+function InputComponent(props: InputProps, ref: Ref<HTMLInputElement>) {
   return (
     <div className={classes.input}>
       <label htmlFor={props.id}>{props.label}</label>
-      <input type="number" {...props} />
+      <input type="number" ref={ref} {...props} />
     </div>
   );
 }
+
+export const Input = forwardRef(InputComponent);
--- src/components/MealItemForm/index.tsx
+++ src/components/MealItemForm/index.tsx
@@ -1,13 +1,26 @@
+import { useRef, useState } from "react";
 import { Input } from "../Input";
 import type { HTMLProps } from "react";
 import classes from "./index.module.css";
 
 export function MealItemForm() {
-  const clickHandler: HTMLProps<HTMLButtonElement>["onClick"] = (event) =>
+  const InputRef = useRef<HTMLInputElement | null>(null);
+  const [isAmountValid, setIsAmountValid] = useState(true);
+
+  const onSubmitHandler: HTMLProps<HTMLFormElement>["onSubmit"] = (event) => {
     event.preventDefault();
+    const amount = Number.parseFloat(InputRef.current?.value ?? "");
+    if (amount < 1 || amount > 10) {
+      setIsAmountValid(false);
+    } else {
+      setIsAmountValid(true);
+    }
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={onSubmitHandler}>
       <Input
+        ref={InputRef}
         label="Amount"
         id="Amount"
         type="number"
@@ -16,7 +29,10 @@ export function MealItemForm() {
         defaultValue="1"
         step="1"
       />
-      <button onClick={clickHandler}>+ Add</button>
+      <button>+ Add</button>
+      {isAmountValid === false ? (
+        <p>Please! Enter a valid amount.</p>
+      ) : undefined}
     </form>
   );
 }
