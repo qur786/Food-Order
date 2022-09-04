0e233fc0c04130d45c0b944bf631a2ac63d17805
Qurban Ahmad <ahmad76qurban@gmail.com>
2022-09-04 11:50:23 +0530

    feat: use useReducer state

--- src/components/CartContextProvider/index.tsx
+++ src/components/CartContextProvider/index.tsx
@@ -1,13 +1,67 @@
-import { PropsWithChildren } from "react";
+import { useReducer } from "react";
 import { CartContext } from "../Contexts";
+import type { PropsWithChildren, Reducer } from "react";
+import type { CartItem } from "../Cart";
+
+interface AddItemAction {
+  type: "add-item";
+  item: CartItem;
+}
+
+interface RemoveItemAction {
+  type: "remove-item";
+  id: string;
+}
+
+type DispatchCartAction = AddItemAction | RemoveItemAction;
+
+interface CartState {
+  items: CartItem[];
+  totalAmount: number;
+}
+
+const defaultCartState: CartState = {
+  items: [],
+  totalAmount: 0,
+};
+
+const cartReducer: Reducer<CartState, DispatchCartAction> = (
+  prevState,
+  action
+) => {
+  let updatedState = defaultCartState;
+  if (action.type === "add-item") {
+    const updatedItems = prevState.items.concat(action.item);
+    updatedState = {
+      ...updatedState,
+      items: updatedItems,
+      totalAmount:
+        prevState.totalAmount + action.item.price * action.item.amount,
+    };
+  }
+  return updatedState;
+};
 
 export function CartContextProvider({ children }: PropsWithChildren) {
-  const addItemHandler: CartContext["addItem"] = () => {};
-  const removeItemHandler: CartContext["removeItem"] = () => {};
+  const [state, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
+
+  const addItemHandler: CartContext["addItem"] = (item) => {
+    dispatchCartAction({
+      type: "add-item",
+      item,
+    });
+  };
+
+  const removeItemHandler: CartContext["removeItem"] = (id) => {
+    dispatchCartAction({
+      type: "remove-item",
+      id,
+    });
+  };
 
   const cartConext: CartContext = {
-    items: [],
-    totalAmount: 0,
+    items: state.items,
+    totalAmount: state.totalAmount,
     addItem: addItemHandler,
     removeItem: removeItemHandler,
   };
